b00652efeb5a2eaa422f0acfd47de9f477cc2311
Alan <alanmannon@gmail.com>
2020-06-30 19:53:25 -0500

    added random, gas and rice pages

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,5 +1,18 @@
 class Api::ProductsController < ApplicationController
   def sample_product
+    x = Product.count
+    random_number = rand(1..x)
+    @sample_product = Product.find_by(id: random_number)
     render "sample_product.json.jb"
+  end
+
+  def gasoline
+    @gas = Product.first
+    render "gasoline.json.jb"
+  end
+
+  def rice
+    @rice = Product.find_by(id: 4)
+    render "rice.json.jb"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,12 @@ Rails.application.routes.draw do
   namespace :api do
     get "/sample_product" => "products#sample_product"
   end
+
+  namespace :api do
+    get "/gasoline" => "products#gasoline"
+  end
+
+  namespace :api do
+    get "/rice" => "products#rice"
+  end
 end
