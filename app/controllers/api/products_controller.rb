855b020373eeae9b7c3098d71bba2a9256a270f5
Alan <alanmannon@gmail.com>
2020-06-30 19:35:32 -0500

    set up route for sample_product page

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,5 @@
 class Api::ProductsController < ApplicationController
+  def sample_product
+    render "sample_product.json.jb"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,8 @@ Rails.application.routes.draw do
   # namespace :api do
   #   get "/photos" => "photos#index"
   # end
+
+  namespace :api do
+    get "/sample_product" => "products#sample_product"
+  end
 end
