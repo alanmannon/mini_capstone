71e51d48de4117e7499f3a79d6443d2f7db58c82
Alan <alanmannon@gmail.com>
2020-07-06 20:55:59 -0500

    dry-ed up model methods

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,18 +1,13 @@
 class Product < ApplicationRecord
   def is_discounted?
-    if price > 10
-      discount = "no"
-    elsif price < 10
-      discount = "yes"
-    end
-    discount
+    price < 10
   end
 
   def tax
-    @sales_tax = price * (0.09)
+    sales_tax = price * (0.09)
   end
 
   def total
-    total_price = price + @sales_tax
+    total_price = price + tax
   end
 end
