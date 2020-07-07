ed6256da3d007f61582e02d2950096213d5097c7
Alan <alanmannon@gmail.com>
2020-07-06 20:40:58 -0500

    added discount?, tax, and total methods

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,18 @@
 class Product < ApplicationRecord
+  def is_discounted?
+    if price > 10
+      discount = "no"
+    elsif price < 10
+      discount = "yes"
+    end
+    discount
+  end
+
+  def tax
+    @sales_tax = price * (0.09)
+  end
+
+  def total
+    total_price = price + @sales_tax
+  end
 end
