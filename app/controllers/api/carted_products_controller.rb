d8cf06bc5804bd5684c837f976558861206a86ab
Alan <alanmannon@gmail.com>
2020-07-20 18:32:11 -0500

    added create and index actions in carted_products controller

--- app/controllers/api/carted_products_controller.rb
+++ app/controllers/api/carted_products_controller.rb
@@ -1,2 +1,19 @@
 class Api::CartedProductsController < ApplicationController
+  before_action :authenticate_user
+
+  def create
+    @cart = CartedProduct.new(
+      user_id: current_user.id,
+      product_id: params[:product_id],
+      quantity: params[:quantity],
+    )
+    @cart.save
+    render "show.json.jb"
+  end
+
+  def index
+    @cart = CartedProduct.all
+    @cart = CartedProduct.all.where(user_id: current_user.id)
+    render "index.json.jb"
+  end
 end
